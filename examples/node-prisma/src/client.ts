1f65bbc3d4704abac101e4303545c17b45fadcc7
m10rten <qmaartenenzo@gmail.com>
2023-10-10 15:48:39 +0200

    initial example usage + initial core setup

--- examples/node-prisma/src/client.ts
+++ examples/node-prisma/src/client.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import { zq } from "zod-query";
+// import { adapter } from "zod-query/adapters/prisma";
 
 export const prisma = new PrismaClient();
+
+// export const b: any = zq({ adapter: adapter(prisma) });
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,49 @@
+import { z } from "zod";
+
 // eslint-disable-next-line no-console
 console.info(`
   Thanks for installing 'zod-query'!
   This is the 0.0.4 version with no functionality.
 `);
+
+interface IAdapter<C, Z> {
+  // C = Client, Z = Zod
+  toZod: (input: C) => Z; // magically go from client to zod
+}
+
+type Options<T> = {
+  adapter: Adapter<T>;
+};
+
+class PrismaAdapter<C> implements IAdapter<C, z.AnyZodObject> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  toZod(_input: C) {
+    // C is client of prisma
+    return z.object({});
+  }
+}
+
+class DefaultAdapter<C> implements IAdapter<C, z.AnyZodObject> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  toZod() {
+    return z.object({});
+  }
+}
+
+type Adapter<T> = PrismaAdapter<T>;
+
+// The client we make easy to use
+export type ZodQuery<T> = {
+  get: () => T;
+};
+
+export const zq = <T>(options?: Options<T>) => {
+  // eslint-disable-next-line no-console
+  console.log(options);
+  const adapter = options?.adapter ?? new DefaultAdapter<T>();
+  const schemas = adapter.toZod({} as T);
+
+  return {
+    get: () => schemas,
+  };
+};
